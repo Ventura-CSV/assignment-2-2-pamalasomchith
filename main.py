1327aaa369dafdcfaa8419566a95f24dee819de8
Kyu Lee <kwooler@gmail.com>
2022-12-19 00:51:18 -0800

    A22

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-    C = int(input('Enter the Celcius degree: '))
-    F = (9 / 5) * C + 32.0
-    print(f'Farenheit Degree: {F:10.2f}')
     pass
 
 
