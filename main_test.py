edf84c2954c933265622596fb9c8f0527b191871
Kyu Lee <kwooler@gmail.com>
2023-01-17 11:35:42 -0800

    A22

--- main_test.py
+++ main_test.py
@@ -16,7 +16,7 @@ def test_main_1():
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
-    res = re.search('[\w,\W]*73\.40[\w,\W]*', lines[0])
+    res = re.search(r'[\w,\W]*73\.40[\w,\W]*', lines[0])
     assert res != None
     print(res.group())
 
@@ -33,6 +33,6 @@ def test_main_2():
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
-    res = re.search('[\w,\W]*95[\w,\W]*', lines[0])
+    res = re.search(r'[\w,\W]*95[\w,\W]*', lines[0])
     assert res != None
     print(res.group())
